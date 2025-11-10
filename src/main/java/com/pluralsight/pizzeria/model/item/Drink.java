f0b54b799ecefa33c2dc70a249b1b8c5007d52e9
Nauman <naumansqb@gmail.com>
2025-11-10 14:22:59 -0500

    Implemented Drink.java - Uses Map to store size with its respective price - Calculates price based on qty and size

--- src/main/java/com/pluralsight/pizzeria/model/item/Drink.java
+++ src/main/java/com/pluralsight/pizzeria/model/item/Drink.java
@@ -1,14 +1,54 @@
 package com.pluralsight.pizzeria.model.item;
 
-public class Drink implements Item{
+import java.util.Map;
+
+public class Drink implements Item {
+    private String size;
+    private String flavor;
+    private int qty;
+
+    private static final Map<String, Double> SIZE_PRICES = Map.of(
+            "small", 2.00,
+            "medium", 2.50,
+            "large", 3.00);
+
+    public Drink(String size, String flavor, int qty) {
+        this.size = size;
+        this.flavor = flavor;
+        this.qty = qty;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
 
     @Override
     public double calculatePrice() {
-        return 0;
+        return qty * SIZE_PRICES.get(size.toLowerCase());
     }
 
     @Override
     public String getDescription() {
-        return "";
+        return qty + ": " + size + " " + flavor;
     }
 }
