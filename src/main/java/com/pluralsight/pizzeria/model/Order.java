c5ad213e21ae1e425025947bdc1bb3042be6a5c9
Nauman <naumansqb@gmail.com>
2025-11-11 13:34:14 -0500

    Implemented Order.java - Calculates total price based on all items - Generic order summary (temporary)

--- src/main/java/com/pluralsight/pizzeria/model/Order.java
+++ src/main/java/com/pluralsight/pizzeria/model/Order.java
@@ -1,4 +1,63 @@
 package com.pluralsight.pizzeria.model;
 
+import com.pluralsight.pizzeria.model.item.Item;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+
 public class Order {
+    private String receiptNumber;
+    private LocalDateTime orderPlacedTime;
+    private List <Item> items;
+
+    public Order(String receiptNumber, LocalDateTime orderPlacedTime) {
+        this.orderPlacedTime = LocalDateTime.now();
+        this.receiptNumber = orderPlacedTime.format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+        this.items = new ArrayList<>();
+    }
+
+    public String getReceiptNumber() {
+        return receiptNumber;
+    }
+
+    public void setReceiptNumber(String receiptNumber) {
+        this.receiptNumber = receiptNumber;
+    }
+
+    public LocalDateTime getOrderPlacedTime() {
+        return orderPlacedTime;
+    }
+
+    public void setOrderPlacedTime(LocalDateTime orderPlacedTime) {
+        this.orderPlacedTime = orderPlacedTime;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+    public void removeItem(Item item){
+        items.remove(item);
+    }
+
+    public double calculateTotal(){
+        return items.stream().mapToDouble(Item::calculatePrice).sum();
+    }
+
+    public String orderSummary() {
+        return "Order{" +
+                "receiptNumber='" + receiptNumber + '\'' +
+                ", orderPlacedTime=" + orderPlacedTime +
+                ", items=" + items +
+                '}';
+    }
 }
